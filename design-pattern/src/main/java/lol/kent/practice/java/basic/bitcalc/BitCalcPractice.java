81eba995855dd3fba87d37b7de0c72e6d837e22f
Shunqin.Chen <kentchensq@gmail.com>
2019-04-09 20:05:15 +0800

    feat: 位运算测试

---
+++ design-pattern/src/main/java/lol/kent/practice/java/basic/bitcalc/BitCalcPractice.java
@@ -0,0 +1,40 @@
+package lol.kent.practice.java.basic.bitcalc;
+
+/**
+ * 标题、简要说明. <br>
+ * Java位运算测试
+ * <p>
+ * Copyright: Copyright (c) 2019年04月09日 17:25
+ * <p>
+ * Company: AMPM Fit
+ * <p>
+ *
+ * @author Shunqin.Chen
+ * @version x.x.x
+ */
+public class BitCalcPractice {
+
+    public static void main(String[] args) {
+        int number = 125;
+        int result = 0;
+        System.out.println("原始二进制数："+Integer.toBinaryString(number));
+        System.out.println("==========================================");
+        //原始数二进制
+        result = number << 1;
+        //左移一位
+        printInfo(result);
+        //复位
+        result = number >> 1;
+        //右移一位
+        printInfo(result);
+    }
+
+    /**
+     * 输出一个int的二进制数
+     *
+     * @param result 移位计算后的结果
+     */
+    private static void printInfo(int result) {
+        System.out.println("移位后的结果:"+Integer.toBinaryString(result));
+    }
+}
