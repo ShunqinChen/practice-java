cae40b29e42249acc85b153ec7dd0d49399d6bbe
Shunqin.Chen <kentchensq@gmail.com>
2019-12-26 15:21:30 +0800

    feat: joda time 时间常量测试

---
+++ libraries/joda-time/src/main/java/lol/kent/practice/jodatime/ConstantsTest.java
@@ -0,0 +1,25 @@
+package lol.kent.practice.jodatime;
+
+import org.joda.time.DateTimeConstants;
+
+/**
+ * <pre>
+ *    类描述:
+ * </pre>
+ * <p>
+ * Copyright: Copyright (c) 2019年12月26日 15:19
+ * <p>
+ * Company: AMPM Fit
+ * <p>
+ *
+ * @author Shunqin.Chen
+ * @version 1.0.0
+ */
+public class ConstantsTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("一天多少个小时:" + DateTimeConstants.HOURS_PER_DAY);
+    }
+
+}
