3d9ed2d18335af540022823d6e4516717419cca1
kentchensq@gmail.com <rX14481946>
2023-05-07 20:15:31 +0800

    feat: test dateTimeZone, time format print

--- libraries/joda-time/src/main/java/lol/kent/practice/jodatime/Application.java
+++ libraries/joda-time/src/main/java/lol/kent/practice/jodatime/Application.java
@@ -1,7 +1,9 @@
 package lol.kent.practice.jodatime;
 
+import com.google.common.collect.Lists;
+import java.util.List;
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
+import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 
@@ -22,16 +24,36 @@ public class Application {
 
 
     public static void main(String[] args) {
-        DateTime dateTime = new DateTime();
+        DateTime dateTime = new DateTime(DateTimeZone.UTC);
+        System.out.println(dateTime.toString("yyyyMMdd"));
+        System.out.println(String.format("water:%s:usr:643f992d0f60235e492fe6ca", dateTime.toString("yyyyMMdd")));
         System.out.println(dateTime.getYear());
         System.out.println(dateTime.getMonthOfYear());
         System.out.println(dateTime.getHourOfDay() + ":" + dateTime.getMinuteOfHour());
 
         DateTimeFormatter fmt = ISODateTimeFormat.dateTime();
-        DateTimeFormatter fmt2 = DateTimeFormat
-                .forPattern(java.time.format.DateTimeFormatter.ISO_DATE.toString());
+//        DateTimeFormatter fmt2 = DateTimeFormat
+//                .forPattern(java.time.format.DateTimeFormatter.ISO_DATE.toString());
 
-        System.out.println(fmt.print(dateTime));
+//        System.out.println(fmt.print(dateTime));
+
+        DateTime date = DateTime.now();
+        DateTime dateAfterOneWeek = date.plusWeeks(1);
+        List<DateTime> weekDate = Lists.newArrayList();
+        while (date.isBefore(dateAfterOneWeek)) {
+            weekDate.add(date);
+            date = date.plusDays(1);
+        }
+
+        System.out.println(weekDate.get(6).toDateTimeISO());
+
+        DateTime b = new DateTime();
+        System.out.println(b.dayOfWeek().get());
+        b = b.withYear(2023).withDayOfWeek(1).withWeekOfWeekyear(20);
+        System.out.println(b);
+        b = b.withYear(2023).withDayOfWeek(7).withWeekOfWeekyear(20);
+        System.out.println(b);
+
     }
 
 }
